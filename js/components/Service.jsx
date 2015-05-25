7968a573e4df72a8dcbf0bdaadbf2ac1ae5ce181
Kent English <kent.english@gmail.com>
2015-05-25 16:05:09 -0400

    Better spacing of columns

--- js/components/Service.jsx
+++ js/components/Service.jsx
@@ -12,7 +12,7 @@ var Service = React.createClass({
                 <div className="col-3-12">
                     <div className="service-label">{this.props.service.label}</div>
                 </div>
-                <div className="col-3-12">
+                <div className="col-4-12">
                     <a className="service-url" href={this.props.service.url} target="_blank">
                         {this.props.service.url}
                     </a>
@@ -20,7 +20,7 @@ var Service = React.createClass({
                 <div className="col-3-12">
                     <div className="service-display">{this.props.service.display}</div>
                 </div>
-                <div className="col-2-12">
+                <div className="col-1-12">
                     <div className="service-last-seen">
                         {ServiceStore.getLastSeen(this.props.service.label)}
                     </div>
